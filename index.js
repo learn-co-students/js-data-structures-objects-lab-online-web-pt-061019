f8d8f8d085f549549768a5f1967eade9af41762a
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-05-12 23:39:35 -0700

    Done.

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 const driver = {}
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  const newDriver = {...driver}
-  newDriver[key] = value
-  return newDriver
+  // const newDriver = {...driver}
+  // newDriver[key] = value
+  // return newDriver
+  return Object.assign({}, driver, {[key]: value});
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
@@ -13,12 +14,15 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function deleteFromDriverByKey(driver, key) {
-  let newDriver = {...driver}
-  delete newDriver[key]
-  return newDriver
+  // let newDriver = {...driver}
+  // delete newDriver[key]
+  // return newDriver
+  const updatedDriver = Object.assign({}, driver, [key]);
+  delete updatedDriver[key]
+  return updatedDriver;
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
-  delete driver[key]
-  return driver
+  delete driver[key];
+  return driver;
 }
